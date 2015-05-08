d59fd35c521fc5570fe0f8601c05360fcd8d3e1f
Andreas Klinger <andreas@klinger.io>
2015-05-08 16:54:56 -0700

    Introduce custom credits

--- asteroids.js
+++ asteroids.js
@@ -734,31 +734,13 @@ function Asteroids() {
 		this.highscoreLink.href = '#';
 		this.highscoreLink.innerHTML = "Submit highscore";
 		this.navigation.appendChild(this.highscoreLink);
-		
-		this.appstore = document.createElement('div');
-		with ( this.appstore.style ) {
-			position = 'fixed';
-			top = '10px';
-			right = '10px';
-			zIndex = '9999999';
-		}
-		this.appstore.className = 'ASTEROIDSYEAH';
-		this.appstore.innerHTML = '<a class="ASTEROIDSYEAH" target="_blank" href="http://itunes.apple.com/us/app/kick-ass-destroy-the-web/id436623109?mt=8&ls=1"><img src="http://erkie.github.com/appstore.png" class="ASTEROIDSYEAH" style="border: none" alt="Get the mobile version" /></a>';
-		this.appstore.getElementsByTagName('a')[0].onclick = function() {
-			this.parentNode.removeChild(this);
-		}
-		document.body.appendChild(this.appstore);
-		
-		// fb like box
-		this.fbLike = document.createElement('div');
-		this.fbLike.innerHTML = '<iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FKick-Ass-Destroy-the-web%2F168200253236727&amp;width=292&amp;colorscheme=light&amp;show_faces=false&amp;stream=false&amp;header=false&amp;height=62" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:300px; height:70px;" allowTransparency="true"></iframe>';
-		this.navigation.appendChild(this.fbLike);
-		
-		// Don't show appstore on frontpage, because they are already present
-		if ( document.location.href === 'http://erkie.github.com/' ) {
-			this.appstore.style.display = "none";
-		}
-		
+
+
+		// credits for kickass
+		this.credits = document.createElement('div');
+		this.credits.innerHTML = '<div style="padding: 15px 20px 0"><span style="color: red; font-size: 150%">♥</span> this asteroids game? <br><span style="margin-top: 10px; font-size: smaller; color: #666">It\'s made by the awesome folks of <a target="_blank" href="http://kickassapp.com">KICK ASS</a> App. <br>Checkout their game in the <a href="http://itunes.apple.com/us/app/kick-ass-destroy-the-web/id436623109?mt=8" target="_blank">App Store</a>.</span></div>';
+		this.navigation.appendChild(this.credits);
+
 		this.highscoreLink.onclick = function() {
 			if ( ! that.highscores ) {
 				that.highscores = new Highscores();
@@ -1196,7 +1178,6 @@ function Asteroids() {
 		if ( this.highscores )
 			this.highscores.hide();
 		this.gameContainer.parentNode.removeChild(this.gameContainer);
-		this.appstore.parentNode.removeChild(this.appstore);
 	};
 }
 
